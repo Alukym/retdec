3753a091cc7547ad5daed043af27903e0e158ed1
RikkaRin <100074184+Alukym@users.noreply.github.com>
2025-01-30 00:12:27 +0800

    Update install-deps.sh

--- .github/workflows/Linux/install-deps.sh
+++ .github/workflows/Linux/install-deps.sh
@@ -2,7 +2,8 @@
 
 set -x
 
+sudo add-apt-repository universe
 sudo apt-get update
-sudo apt-get install openssl gcc-multilib python3-venv doxygen graphviz libncurses-dev
+sudo apt-get install openssl gcc-multilib python3-venv doxygen graphviz libncurses-dev libtinfo5
 
 doxygen --version
